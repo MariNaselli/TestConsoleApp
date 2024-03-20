ccbac51d3b79794f43641bcbce8541f08e2dcac0
Mariana Naselli <nasellimariana@gmail.com>
2024-03-20 14:57:11 +0100

    Merge branch develop_class_library into master

---
+++ ArrayManagement.Tests.ClassLibrary/ArraySorterTestClassLibrary.cs
@@ -0,0 +1,58 @@
+using ArrayManagementClassLibrary;
+
+namespace ArrayManagement.Tests.ClassLibrary
+{
+    public class ArraySorterTestClassLibrary
+    {
+        [Fact]
+        public void TestFindIndexOfMin()
+        {
+            // Arrange
+            int[] array = { 5, 3, 9, 2, 7 };
+            int fromIndex = 0;
+            int toIndex = array.Length - 1;
+
+            // Act
+            int result = ArraySorterClassLibrary.FindIndexOfMin(array, fromIndex, toIndex);
+
+            // Assert
+            Assert.Equal(3, result); // Esperamos que el índice del valor mínimo sea 3 (el valor 2)
+        }
+
+        [Fact]
+
+        public void TestSwapElements()
+        {
+            // Arrange
+            int[] array = { 5, 3, 9, 2, 7 };
+            int valorPosicion0 = 3;
+            int valorPosicion1 = 5;
+            // Act 
+            ArraySorterClassLibrary.SwapElements(array, 0, 1);
+
+            // Assert
+            Assert.Equal(valorPosicion0, array[0]);
+            Assert.Equal(valorPosicion1, array[1]);
+
+        }
+
+        [Fact]
+
+        public void TestSortArray()
+        {
+            // Arrange
+            int[] array = { 5, 3, 9, 2, 7 };
+            int[] array_esperado = { 2, 3, 5, 7, 9 };
+
+            // Act 
+            ArraySorterClassLibrary.SortArray(array);
+
+
+            // Assert
+            Assert.Equal(array_esperado, array);
+
+
+        }
+    }
+
+}
---
+++ ArrayManagementClassLibrary/ArraySorterClassLibrary.cs
@@ -0,0 +1,95 @@
+﻿namespace ArrayManagementClassLibrary
+{
+    public class ArraySorterClassLibrary
+    {
+        public static int FindIndexOfMin(int[] array, int fromIndex, int toIndex)
+        {
+            int result = -1;
+            //Validamos que el array no està vacio
+            if (array.Length != 0)
+            {
+                if (fromIndex >= 0)
+                {
+                    if (toIndex < array.Length)
+                    {
+                        if (fromIndex <= toIndex)
+                        {
+                            //se cumple todo
+                            int valorMinimo = array[fromIndex];
+                            int posicionValorMinimo = fromIndex;
+                            for (int i = fromIndex; i <= toIndex; i++)
+                            {
+                                int valorActual = array[i];
+                                if (valorActual < valorMinimo)
+                                {
+                                    valorMinimo = valorActual;
+                                    posicionValorMinimo = i;
+                                }
+                            }
+                            result = posicionValorMinimo;
+
+                            Console.WriteLine("El valor minimo del array: [" + string.Join(", ", array) + "] es " + valorMinimo);
+                            Console.WriteLine("La posiciòn del valor minimo en el array es " + posicionValorMinimo);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("is not fromIndex ≤ toIndex");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("is not toIndex < n");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("is not fromIndex ≥ 0");
+                }
+            }
+            else
+            {
+                ///el array esta vacio
+                Console.WriteLine("El array está vacio");
+            }
+            return result;
+        }
+
+        public static void SwapElements(int[] array, int i, int j)
+        {
+            Console.WriteLine("Array antes de invertir los elementos: [" + string.Join(", ", array) + "]");
+
+            int valorPosicionI = array[i];
+            int valorPosicionJ = array[j];
+
+            array[i] = valorPosicionJ;
+            array[j] = valorPosicionI;
+
+            Console.WriteLine("Array despues de invertir los elementos: [" + string.Join(", ", array) + "]");
+
+        }
+
+        public static void SortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int toIndex = array.Length - 1;
+                int miIndex = FindIndexOfMin(array, i, toIndex);
+                SwapElements(array, i, miIndex);
+            }
+
+            Console.WriteLine("Array ordenado de forma creciente: [" + string.Join(", ", array) + "]");
+        }
+
+        //public static void SortArray(int[] array)
+        //{
+        //    Console.WriteLine("Array: [" + string.Join(", ", array) + "]");
+
+        //    Array.Sort(array);
+
+        //    Console.WriteLine("Array ordenado de forma creciente: [" + string.Join(", ", array) + "]");
+        //}
+
+    }
+}
